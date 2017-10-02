64d31a9b978f6e305231a4748531cea48bbccd39
Sam Levene <sam.levene@yahoo.co.uk>
2017-10-02 09:16:05 +0100

    Update rightmovelogin.rb

--- lib/pages/rightmovelogin.rb
+++ lib/pages/rightmovelogin.rb
@@ -2,9 +2,9 @@ class Pages::Rightmovelogin < SitePrism::Page
 
 	def enterValidDetails
 		#Input vaid data here
-		iterator = "7" #Change this to repeat the test
-
-		fill_in('email', :with => 'nodkane12+'+iterator+'@gmail.com')
+		iterator = "1" #Change this to repeat the test
+		
+		fill_in('email', :with => 'joe.bloggs+'+iterator+'@gmail.com')
 		fill_in('password', :with => 'password')
 		click_button('Sign in')
 	end
